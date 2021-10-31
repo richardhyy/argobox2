da54804b68398a8f91a0bc43955bd9875f6565f2
Alan_Richard <7609740+richardhyy@users.noreply.github.com>
2021-10-31 10:31:54 +0800

    Populate ArgoHeader (meta info) from Core Profile

--- argo_importer/argocore.py
+++ argo_importer/argocore.py
@@ -1,17 +1,35 @@
 from collections import OrderedDict
 from os import listdir
+from datetime import datetime
+from django.utils import timezone
+
+datetime_format = '%Y%m%d%H%M%S'
+date_format = '%Y-%m-%d'
 
 
-def profile_data_to_json(path):
+def parse_profile_data(path):
     def remove_space(text):
         return text.replace(' ', '')
 
     def contain_ignore_space(full, sub):
         return remove_space(sub) in full
 
-    entry = {}
-
     # spaces will be removed automatically
+    meta_prefix_field_dict = {
+        'PLATFORM NUMBER': 'platform_number',
+        'CYCLE NUMBER': 'cycle_number',
+        'DATE CREATION': 'date_creation',
+        'PROJECT NAME': 'project_name',
+        'PI NAME': 'pi_name',
+        'INSTRUMENT TYPE': 'instrument_type',
+        'SAMPLE DIRECTION': 'sample_direction',
+        'DATA MODE': 'data_mode',
+        'JULIAN DAY': 'julian_day',
+        'DATE': 'date',
+        'LATITUDE': 'latitude',
+        'LONGITUDE': 'longitude'
+    }
+
     column_header_prefix_field_dict = {
         'Pressure (dbar)': 'pressure',
         'Corrected Pressure (dbar)': 'cpressure',
@@ -28,6 +46,11 @@ def profile_data_to_json(path):
 
     file = open(path, 'r')
     lines = file.readlines()
+
+    out_entries = {
+        'meta': {},
+        'data': {}
+    }
 
     data_table = {}
 
@@ -54,13 +77,20 @@ def profile_data_to_json(path):
                 data_table[i].append(_value)
         else:
             line = remove_space(line)
-            if contain_ignore_space(line, 'PLATFORM NUMBER'):
+            contains_meta = False
+            for key, value in meta_prefix_field_dict.items():
+                # if the line contains the meta info we need
                 split = line.split(':', 1)
-                entry['platform_number'] = split[1]
-            elif contain_ignore_space(line, 'CYCLE NUMBER'):
-                split = line.split(':', 1)
-                entry['cycle_number'] = split[1]
-            elif contain_ignore_space(line, 'COLUMN'):
+                if split[0] == remove_space(key):
+                    # Get string on the lhs of '('
+                    # Example (We need 'A' here):
+                    #     SAMPLE DIRECTION      :A(A=Ascend; D=Descend)
+                    if '(' in split[1]:
+                        split[1] = split[1].split('(', 1)[0]
+                    out_entries['meta'][value] = split[1]
+                    contains_meta = True
+                    break
+            if not contains_meta and contain_ignore_space(line, 'COLUMN'):
                 # Example:
                 #     COLUMN 1              :Pressure (dbar)                             F7.1
                 split = line.split(':', 1)
@@ -78,9 +108,9 @@ def profile_data_to_json(path):
     for col_index in column_index_field_dict.keys():
         field = column_index_field_dict[col_index]
         list_str = '{' + (','.join(data_table[col_index])) + '}'
-        entry[field] = list_str
+        out_entries['data'][field] = list_str
 
-    return entry
+    return out_entries
 
 
 if __name__ == "__main__":
@@ -103,32 +133,66 @@ if __name__ == "__main__":
     path = input("Path to Argo core profile folder: ").replace('\'', '')
     files = [f for f in listdir(path) if isfile(join(path, f))]
 
-    entries = []
+    print("Parsing data...")
+
+    profile_entries = []
     for file in files:
-        entries.append(profile_data_to_json(path + "/" + file))
+        profile_entries.append(parse_profile_data(path + "/" + file))
 
-    print("{} meta files loaded".format(len(entries)))
+    print("{} profile files loaded".format(len(profile_entries)))
 
-    for entry in entries:
-        if models.ArgoCore.objects.filter(platform_number=entry['platform_number'],
-                                          cycle_number=entry['cycle_number']).exists():
+    for entry in profile_entries:
+        # Save meta
+        meta_entry = entry['meta']
+        if models.ArgoHeader.objects.filter(platform_number=meta_entry['platform_number'],
+                                            cycle_number=meta_entry['cycle_number']).exists():
             # pass if the record already exists
-            print("{}@{} - already exists".format(entry['platform_number'], entry['cycle_number']))
+            print("Header {}@{} - already exists".format[meta_entry['platform_number'], meta_entry['cycle_number']])
             continue
 
-        e = models.ArgoCore(
-            platform_number=entry['platform_number'],
-            cycle_number=entry['cycle_number'],
-            pressure=entry['pressure'],
-            cpressure=entry['cpressure'],
-            qpressure=entry['qpressure'],
-            temperature=entry['temperature'],
-            ctemperature=entry['ctemperature'],
-            qtemperature=entry['qtemperature'],
-            salinity=entry['salinity'],
-            csalinity=entry['csalinity'],
-            qsalinity=entry['qsalinity']
-        )
-        e.save()
+        try:
+            e = models.ArgoHeader(
+                platform_number=meta_entry['platform_number'],
+                cycle_number=meta_entry['cycle_number'],
+                date_creation=datetime.strptime(meta_entry['date_creation'], datetime_format),
+                project_name=meta_entry['project_name'],
+                pi_name=meta_entry['pi_name'],
+                instrument_type=meta_entry['instrument_type'],
+                sample_direction=meta_entry['sample_direction'],
+                data_mode=meta_entry['data_mode'],
+                julian_day=meta_entry['julian_day'],
+                date=datetime.strptime(meta_entry['date'], date_format),
+                latitude=meta_entry['latitude'],
+                longitude=meta_entry['longitude'],
+            )
+            e.save()
+        except Exception as ex:
+            raise ex
+
+        # Save profile data
+        data_entry = entry['data']
+        if models.ArgoCore.objects.filter(platform_number=meta_entry['platform_number'],
+                                          cycle_number=meta_entry['cycle_number']).exists():
+            # pass if the record already exists
+            print("Core profile {}@{} - already exists".format(meta_entry['platform_number'], meta_entry['cycle_number']))
+            continue
+
+        try:
+            e = models.ArgoCore(
+                platform_number=meta_entry['platform_number'],
+                cycle_number=meta_entry['cycle_number'],
+                pressure=data_entry['pressure'],
+                cpressure=data_entry['cpressure'],
+                qpressure=data_entry['qpressure'],
+                temperature=data_entry['temperature'],
+                ctemperature=data_entry['ctemperature'],
+                qtemperature=data_entry['qtemperature'],
+                salinity=data_entry['salinity'],
+                csalinity=data_entry['csalinity'],
+                qsalinity=data_entry['qsalinity']
+            )
+            e.save()
+        except Exception as ex:
+            raise ex
 
     print("Done")
